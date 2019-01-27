0fd4b82f6390d99c80a3fbf0374c5a741c80e0f9
neyser zana <neyserj.zana@gmail.com>
2019-01-26 23:05:47 -0500

    adding the new create directory object function into the tree commit

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -11,36 +11,67 @@ class Tree extends Component {
 
   //* Creates directory object. With some parent-child relationship. Rush version. Does not handle duplicate childs.
   createDirectoryObject(array, directory = {}) {
-    array.forEach((element, i) => {
-      let arr = element.split('/');
-      for (let i = 1; i < arr.length; i++) {
-        if (directory[arr[1]] !== undefined) {
-          directory = this.directoryDepth(arr, directory, i);
+    return array.reduce((acc, curr, index, array) => {
+      curr = curr.replace(/\//g, ' ').trim();
+      let arr = curr.split(' ');
+      for (let i = 0; i < arr.length; i++) {
+        if (i > 1) { break; }
+        if (!acc[arr[0]]) {
+          acc[arr[i]] = {
+            name: arr[i],
+            children: []
+          };
         } else {
-          directory[arr[i]] = {
-            name: arr[i]
-          }
+          const arrCopy = arr.slice().splice(1);
+          acc[arr[0]].children.push(
+            ...this.directoryDepthHelper(acc[arr[0]].children, arr[i], arrCopy)
+          );
         }
       }
-    });
-    return directory;
+  
+      return acc;
+    }, {});
   }
-
-  directoryDepth(array, directory, index) {
-    if (directory[array[1]].children === undefined) {
-      let newChild = {
-        name: array[index]
+  
+  directoryDepthHelper(childrenArr, item, currentArr) {
+    return currentArr.reduce((acc, curr, index, currentArr) => {
+      // console.log(curr, currentArr);
+      let childExists = false;
+  
+      if (item === curr) {
+        childExists = this.childExistsHelper(currentArr[0], childrenArr);
+      } else {
+        childExists = this.childExistsHelper(currentArr[0], acc) || this.childExistsHelper(currentArr[0], childrenArr);
       }
-      directory[array[1]].children = [];
-      directory[array[1]].children.push(newChild);
-    } else {
-      let newChild = {
-        name: array[index]
+  
+      if (childExists) {
+        const currentArrCopy = currentArr.slice().splice(1);
+        childExists.children.push(
+          ...this.directoryDepthHelper(childExists.children, curr, currentArrCopy)
+        );
+      } else {
+        const newChild = {
+          name: curr,
+          children: []
+        };
+        acc.push(newChild);
       }
-      directory[array[1]].children.push(newChild);
+  
+      return acc;
+    }, []);
+  }
+  
+  childExistsHelper(element, array) {
+    let child = false;
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].name === element) {
+        child = array[i];
+        return child;
+      }
     }
-
-    return directory;
+  
+    return child;
   }
 
   createNodes(directory) {
